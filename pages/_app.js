91cc86e031947e48bdf6d130dd9bcbbb2a0aa970
Giangvp99 <truonggianvp99@gmai.com>
2021-12-02 01:46:31 +0300

    management state

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import Head from "next/head"
 import SSRProvider from 'react-bootstrap/SSRProvider';
+import AppWrapper from "../store/index"
 import "./global.css"
 export default function MyApp({ Component, pageProps }) {
     const EmptyLayout = ({ children }) => <>{children}</>
@@ -15,8 +16,37 @@ export default function MyApp({ Component, pageProps }) {
         </Head>
         <Layout>
             <SSRProvider>
-                <Component {...pageProps} />
+                <AppWrapper>
+                    <Component {...pageProps} />
+                </AppWrapper>
             </SSRProvider>
         </Layout>
     </>
 }
+
+// import Head from "next/head"
+// import SSRProvider from 'react-bootstrap/SSRProvider';
+// import Store  from "../store/index"
+// import "./global.css"
+// 
+// export default function MyApp({ Component, pageProps }) {
+//     const EmptyLayout = ({ children }) => <>{children}</>
+//     const Layout = Component.Layout || EmptyLayout
+//     return <>
+//         <Head>
+//             <meta name="viewport" content="width=device-width, initial-scale=1" />
+//             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+//             <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+//             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+//             <link rel="icon" href="/images/favicon.ico" type="image/x-icon" />
+//             <script src="https://kit.fontawesome.com/615000b101.js" crossorigin="anonymous"></script>
+//         </Head>
+//         <Layout>
+//             <SSRProvider>
+//                 <Store>
+//                     <Component {...pageProps} />
+//                 </Store>
+//             </SSRProvider>
+//         </Layout>
+//     </>
+// }
--- parts/card-user/index.js
+++ parts/card-user/index.js
@@ -1,10 +1,21 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 import styles from "../../styles/card.module.scss"
+import { useAppContext } from "../../store/index"
 export default function Card(props) {
+    const [state, setState] = useAppContext()
     const Buy = () => {
-        props.setBadge(prev => prev + 1)
+        let i = state.users.findIndex(a => a.name == "Giang")
+        setState({
+            ...state, users: [
+                ...state.users.slice(0, i),
+                { ...state.users[i], age: state.users[i].age + 1 },
+                ...state.users.slice(i + 1)
+            ]
+        })
+        console.log(state.users[i]?.age)
     }
+
     return (<>
         <div className={`${styles['card-product']} mb-3 border border-2 position-relative`}>
             <img src={props.img} className="card-img-top p-4" alt="..." />
@@ -21,4 +32,30 @@ export default function Card(props) {
     </>
     );
 }
-
+// import React, { useContext } from "react";
+// import { Button } from "react-bootstrap";
+// import styles from "../../styles/card.module.scss"
+// import { Context } from "../../store/index"
+// export default function Card(props) {
+//     const [state, dispatch] = useContext(Context)
+//     const Buy = () => {
+//         dispatch({type:"BUY_PRODUCT",payload:state.index+1})
+//         console.log(state.index)
+//     }
+//     return (<>
+//         <div className={`${styles['card-product']} mb-3 border border-2 position-relative`}>
+//             <img src={props.img} className="card-img-top p-4" alt="..." />
+//             <div className="card-body">
+//                 <h5 className="card-title">{props.name}</h5>
+//                 <p className="card-text">{props.description}</p>
+//             </div>
+//             <div className="card-body ms-3 p-1 ">
+//                 <div>Price: {props.price}</div>
+//             </div>
+//             <Button variant="info" onClick={() => Buy()}><i aria-hidden className="fas fa-cart-arrow-down me-1"></i>Buy</Button>{' '}
+//         </div>
+// 
+//     </>
+//     );
+// }
+// 
